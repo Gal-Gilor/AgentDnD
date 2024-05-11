c67134cd7b1b07ac21377aa2241c76bbb4659e8e
Gal Gilor <gilorgal2010@gmail.com>
2024-05-10 22:54:31 -0400

    created text reranking functionalities

--- huggingface_utils.py
+++ huggingface_utils.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from langchain_text_splitters import TokenTextSplitter
 from transformers import (
     AutoModel,
+    AutoModelForSequenceClassification,
     AutoTokenizer,
     PreTrainedModel,
     PreTrainedTokenizerFast,
@@ -101,9 +102,14 @@ class HFEmbedder:
         Returns:
             torch.Tensor: A tensor containing the embeddings.
         """
-
+        config = self.config.get("embedding", {})
+        max_length = config.get("max_length")
         encoded_input = tokenizer(
-            texts, padding=True, truncation=True, return_tensors="pt"
+            texts,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+            max_length=max_length,
         )
 
         model.eval()
@@ -141,11 +147,113 @@ class HFEmbedder:
         return embeddings
 
 
-if __name__ == "__main__":
-    embedder = HFEmbedder(path_to_config="configs/config.yaml")
+class HFReranker:
+    """A class for reranking chunks using Hugging Face transformers.
 
-    # Generate embeddings for multiple texts using multiple threads
-    texts = ["This is a sample text.", "Another example text.", "Yet another text."]
+    Args:
+        path_to_config (str, optional): Path to the configuration file.
+            Defaults to "config.yaml".
 
-    embeddings = embedder.generate_embeddings_multi_thread(texts)
-    logger.info(str(len(embeddings)))
+    Properties:
+        tokenizer (transformers.PreTrainedTokenizer): A tokenizer instance lazily loaded from the configuration.
+        model (transformers.AutoModelForSequenceClassification): A model instance lazily loaded from the configuration.
+
+    Methods:
+        _calcuate_similarity(model, tokenizer, pair) -> torch.Tensor:
+            Generate embeddings for a list of texts using a given model and tokenizer.
+        generate_embeddings_multi_thread(texts) -> list[str]:
+            Generate embeddings for multiple texts using multiple threads.
+
+
+    """
+
+    def __init__(
+        self,
+        path_to_config: Optional[str] = "config.yaml",
+    ):
+        self.config = config_from_file(path_to_config)
+        self._tokenizer = None
+        self._model = None
+
+    @property
+    def tokenizer(self):
+        """Return a tokenizer instance lazily loaded from the configuration.
+
+        Returns:
+            transformers.PreTrainedTokenizer: A tokenizer instance.
+        """
+        if not self._tokenizer:
+            config = self.config.get("reranking", {})
+            model_id = config.get("model_id")
+            if not model_id:
+                raise ValueError(
+                    "Config must include 'reranking.model_id' with a valid HF model ID."
+                )
+            self._tokenizer = AutoTokenizer.from_pretrained(model_id)
+        return self._tokenizer
+
+    @property
+    def model(self):
+        """Return a model instance lazily loaded from the configuration.
+
+        Returns:
+            transformers.PreTrainedModel: A model instance.
+        """
+        if not self._model:
+            config = self.config.get("reranking", {})
+            model_id = config.get("model_id")
+            if not model_id:
+                raise ValueError(
+                    "Config must include 'reranking.model_id' with a valid HF model ID."
+                )
+            self._model = AutoModelForSequenceClassification.from_pretrained(model_id)
+        return self._model
+
+    def _calcuate_similarity(
+        self, model, tokenizer, pair: list[str, str]
+    ) -> torch.Tensor:
+        """ """
+        config = self.config.get("reranking", {})
+        max_length = config.get("max_length")
+        model.eval()
+        query, passage = pair
+
+        with torch.no_grad():
+            inputs = tokenizer(
+                [[query, passage]],
+                padding=True,
+                truncation=True,
+                return_tensors="pt",
+                max_length=max_length,
+            )
+            scores = (
+                model(**inputs, return_dict=True).logits.view(-1,).float()  # fmt: skip
+            )
+
+        return scores
+
+    def calculate_simularity_multi_thread(self, pairs: list[str, str]) -> list[str]:
+        """Generate embeddings for multiple texts using multiple threads.
+
+        Args:
+            texts (List[str]): A list of texts for which embeddings are to be generated.
+
+        Returns:
+            list[str]: A list of embeddings corresponding to the input texts.
+        """
+
+        tokenizer = self.tokenizer
+        model = self.model
+
+        # define the number of threads to use
+        max_threads = os.cpu_count()
+        num_threads = min(max_threads, len(pairs))
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+            # calculate the similarity between the query and the retrieved content
+            def calcuate_similarity(pair):
+                return self._calcuate_similarity(model, tokenizer, pair)
+
+            embeddings = list(executor.map(calcuate_similarity, pairs))
+
+        return embeddings
