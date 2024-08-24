6a66b3c668eeddad814dc23a9c2ee0a78855df8c
Gal Gilor <gilorgal2010@gmail.com>
2024-08-24 19:52:38 -0400

    Removed the __init__ in Settings since pydantic already handles the intialization

--- app/core/settings.py
+++ app/core/settings.py
@@ -22,6 +22,3 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="allow"
     )
-
-    def __init__(self, **data):
-        super().__init__(**data)
